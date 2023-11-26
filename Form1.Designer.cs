75aff881468363e9e8e3baf9ac3db81346a64ff5
Chris99ChangHo <awakeev.d@gmail.com>
2023-11-26 13:32:17 +0900

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace test1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader7 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // listView2
+            // 
+            this.listView2.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader6,
+            this.columnHeader7});
+            this.listView2.HideSelection = false;
+            this.listView2.Location = new System.Drawing.Point(31, 486);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(403, 101);
+            this.listView2.TabIndex = 9;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            this.listView2.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "주문번호";
+            this.columnHeader6.Width = 84;
+            // 
+            // columnHeader7
+            // 
+            this.columnHeader7.Text = "주문상세정보";
+            this.columnHeader7.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader7.Width = 315;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(472, 552);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(109, 35);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "주문취소";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(472, 486);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 37);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "결제완료";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("굴림", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(200, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(208, 23);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "먹거리 주문 대기열";
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(31, 61);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(550, 390);
+            this.listView1.TabIndex = 5;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "주문번호";
+            this.columnHeader1.Width = 90;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "좌석";
+            this.columnHeader2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "아이디";
+            this.columnHeader3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader3.Width = 83;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "메뉴";
+            this.columnHeader4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader4.Width = 212;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "가격";
+            this.columnHeader5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader5.Width = 100;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(605, 607);
+            this.Controls.Add(this.listView2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.ColumnHeader columnHeader7;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Oracle.DataAccess.Client;
+
+namespace test1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            string connectionString = "User Id=chris;Password=1111;Data Source=(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=localhost)(PORT=1521))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=xe)))";
+            OracleConnection myConnection = new OracleConnection(connectionString);
+
+            string commandString = "SELECT MENU_ORDER_ID, PC_SIT_ID, USER_ID, MENU_NAME, MENU_COST FROM MENU_ORDER_LOG JOIN MENU_INFO ON MENU_ORDER_LOG.MENU_ID=MENU_INFO.MENU_ID";
+            OracleCommand myCommand = new OracleCommand();
+            myCommand.Connection = myConnection;
+            myCommand.CommandText = commandString;
+
+            myConnection.Open();
+
+            OracleDataReader MR;
+            MR = myCommand.ExecuteReader();
+
+            while (MR.Read())
+            {
+                ListViewItem item = new ListViewItem(MR[0].ToString());
+                item.SubItems.Add(MR[1].ToString());
+                item.SubItems.Add(MR[2].ToString());
+                item.SubItems.Add(MR[3].ToString());
+                item.SubItems.Add(MR[4].ToString());
+                listView1.Items.Add(item);
+            }
+
+            MR.Close();
+            myConnection.Close();
+
+            listView1.SelectedIndexChanged += ListView1_SelectedIndexChanged;
+        }
+        
+        private void ListView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // 첫 번째 리스트뷰에서 선택된 아이템이 있는지 확인
+            if (listView1.SelectedItems.Count > 0)
+            {
+                // 첫 번째 리스트뷰에서 선택된 아이템의 데이터 가져오기
+                ListViewItem selectedItem = listView1.SelectedItems[0];
+                string selectedOrderID = selectedItem.SubItems[0].Text;
+                string selectedMenuName = selectedItem.SubItems[3].Text;
+
+                // 두 번째 리스트뷰 업데이트
+                UpdateSecondListView(selectedOrderID, selectedMenuName);
+            }
+        }
+
+        private void UpdateSecondListView(string selectedOrderID, string selectedMenuName)
+        {
+            // 중복을 피하기 위해 이미 해당 데이터가 있는지 확인
+            bool isAlreadyAdded = false;
+
+            foreach (ListViewItem item in listView2.Items)
+            {
+                if (item.SubItems[0].Text == selectedOrderID && item.SubItems[1].Text == selectedMenuName)
+                {
+                    isAlreadyAdded = true;
+                    break;
+                }
+            }
+
+            // 중복이 아닌 경우에만 두 번째 리스트뷰에 추가
+            if (!isAlreadyAdded)
+            {
+                ListViewItem newItem = new ListViewItem(new[] { selectedOrderID, selectedMenuName });
+                listView2.Items.Add(newItem);
+            }
+        }
+
+        // 데이터를 뿌려주기 위해서 조인써서 필요한 데이터만 가져오긴 했는데(기존 테이블에도 몇가지 데이터들을 무작위로 넣어서)
+        // 근데 실질적으로 새 테이블을 생성하거나 기존 수정하는게 더 편할듯?
+        // 사용자 PC에서 주문을 해야 넘어올 수 있는 데이터라?? 후행작업부터 한 기분
+        // 일단은 리스트뷰 두개써서 리스트뷰 내에 일렬번호 클릭으로 두 리스트뷰가 상호작용 할 수 있게 생각하고 구현해봄
+        // 주문 상세 내역에 음식 + 음식 + 음식 이렇게 표현하는 방법을 생각하지 못하겠음
+        // 결제는 솔직히 포기... API끌어오는것도 시간 나면 하는게 맞는듯? 일단 버튼으로 구현...
+    }
+}
